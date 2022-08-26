c5b0b0d7bda82fc041f5abaee6e15158db046be8
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-26 15:06:49 +0200

    Working on JS, creating random dice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 console.log('JS OK!')
 
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// Buon lavoro e buon divertimento!
+
+// genera numero casuale tra 1-6 per player
+let playerDice = Math.ceil(Math.random() * 6);
+console.log('player dice value:', playerDice);
+
+// genera numero casuale tra 1-6 per computer
+let computerDice = Math.ceil(Math.random() * 6);
+console.log('computer dice value:', computerDice);
 
